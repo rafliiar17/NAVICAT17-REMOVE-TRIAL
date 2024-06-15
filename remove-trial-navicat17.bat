03ed190b0ba2357f343fe0b7590eff23d1a81b6e
IamRFZ <39895896+rafliiar17@users.noreply.github.com>
2024-06-15 09:27:55 +0700

    add remove shellfolder

--- remove-trial-navicat17.bat
+++ remove-trial-navicat17.bat
@@ -2,22 +2,39 @@
 setlocal enabledelayedexpansion
 
 echo Deleting HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration17XEN...
-
-reg delete "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration17XEN" /f
+reg delete "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration17XEN" /f > nul 2>&1
 
 echo.
 
 echo Deleting parent folders of keys ending with \Info under HKEY_CURRENT_USER\Software\Classes\CLSID...
-
-for /f "tokens=*" %%i in ('REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s ^| findstr /I "\\Info$"') do (
+for /f "tokens=*" %%i in ('REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s /f "Info" ^| findstr /I "\\Info$"') do (
     set "key=%%i"
     set "parent=!key:~0,-5!"
-    echo Deleting !parent!
-    reg delete "!parent!" /f
+    echo Deleting !parent! ...
+    reg delete "!parent!" /f > nul 2>&1
 )
 
 echo.
 
-echo Finished
+echo Deleting parent folders of keys ending with \InprocServer32 under HKEY_CURRENT_USER\Software\Classes\CLSID...
+for /f "tokens=*" %%i in ('REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s /f "InprocServer32" ^| findstr /I "\\InprocServer32$"') do (
+    set "key=%%i"
+    set "parent=!key:~0,-15!"
+    echo Deleting !parent! ...
+    reg delete "!parent!" /f > nul 2>&1
+)
 
+echo.
+
+echo Deleting parent folders of keys ending with \ShellFolder under HKEY_CURRENT_USER\Software\Classes\CLSID...
+for /f "tokens=*" %%j in ('REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s /f "ShellFolder" ^| findstr /I "\\ShellFolder$"') do (
+    set "key=%%j"
+    set "parent=!key:~0,-12!"
+    echo Deleting !parent! ...
+    reg delete "!parent!" /f > nul 2>&1
+)
+
+echo.
+
+echo Finished.
 pause
